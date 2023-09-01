8b37e7ba10b9783032d17a43b151695778dbc2c3
Timmy485 <yeboahtimothy@gmail.com>
2023-09-01 09:08:42 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 st.markdown(
-    "<h3 style='text-align: center; padding: 10px;'>Analyze Sentiment Trends of British Airways Flights</h3>",
+    "<h3 style='text-align: center; padding: 2px;'>Analyze Sentiment Trends of British Airways Flights</h3>",
     unsafe_allow_html=True
 )
 
@@ -69,7 +69,7 @@ st.markdown("</div>", unsafe_allow_html=True)
 
 # Footer with a subdued color
 st.markdown(
-    "<div style='position: fixed; bottom: 0; left: 0; width: 100%; text-align: center; background-color: #f0f0f0; padding: 10px;'>"
+    "<div style='position: fixed; bottom: 0; left: 0; width: 100%; text-align: center;'>"
     "<p style='color: #999;'>AeroReview Explorer by Yeboah Timothy</p>"
     "</div>",
     unsafe_allow_html=True
