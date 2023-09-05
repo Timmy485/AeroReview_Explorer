9c25199f3e2df76bbb43b37ea78df524f69d1298
Timmy485 <yeboahtimothy@gmail.com>
2023-09-05 15:14:07 +0000

    completed sentiment distribution

--- app/app.py
+++ app/app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import plotly.express as px
+import pandas as pd
+import base64
+from collections import Counter
 # from . import config
 
-st. set_page_config(layout ="wide")
+st. set_page_config(layout="wide")
 st.markdown(
     """
     <style>
@@ -13,6 +17,27 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
+
+def set_bg_hack(main_bg):
+    # set bg name
+    main_bg_ext = "png"
+
+    st.markdown(
+        f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover;
+         }}
+         </style>
+         """,
+        unsafe_allow_html=True
+    )
+
+
+set_bg_hack('media/plane.jpg')
+
 # Set the title and subtitle with background color
 st.markdown(
     "<h1 style='text-align: center; padding: 10px;'>AEROREVIEW EXPLORER</h1>",
@@ -22,9 +47,9 @@ st.markdown(
     "<h3 style='text-align: center; padding: 2px;'>Analyze Sentiment Trends of British Airways Flights</h3>",
     unsafe_allow_html=True
 )
-st.image("app/media/robo.jpg",
-             use_column_width=True,
-             )
+# st.image("media/robo.jpg",
+#              use_column_width=True,
+#              )
 
 # Sidebar Navigation with header background color
 st.sidebar.markdown(
@@ -35,10 +60,9 @@ st.sidebar.markdown(
 
 # Use a dropdown menu for feature selection
 selected_feature = st.sidebar.selectbox("Select a Feature", (
-    "Sentiment Distribution", "Data Visualization",  "Word Clouds", 
-    "Topic Modeling", "Real-time Sentiment Analysis", "Geographical Insights", 
+    "Sentiment Distribution", "Data Visualization",  "Word Clouds",
+    "Topic Modeling", "Real-time Sentiment Analysis", "Geographical Insights",
     "Comparative Analysis", "Sentiment Trends by Aspect", "Predictive Insights"))
-
 
 
 # Main Content with light background color
@@ -51,7 +75,42 @@ st.markdown(
 if selected_feature == "Sentiment Distribution":
     # Sentiment Distribution Content
     st.header("Sentiment Distribution")
-    # Include sentiment distribution chart.
+
+    # Sample sentiment data
+    
+    counts = [50, 30, 20]  # Replace with your actual sentiment counts
+
+    df = pd.read_csv("../data/review_sentiment_df.csv")
+    sentiments = ["Positive", "Negative", "Neutral"]
+    count = Counter(df['sentiments'])
+    counts = [count['Positive'], count['Negative'], count['Neutral']]
+
+    # Create a pie chart with custom size
+    fig = px.pie(
+        names=sentiments,
+        values=counts,
+        title="Sentiment Distribution for Verified Reviews",
+        labels={"names": "Sentiment"},
+        width=1000,  # Set the width
+        height=800,  # Set the height
+    )
+    fig.update_layout(paper_bgcolor="rgba(0,0,0,0)",
+                      plot_bgcolor="rgba(0,0,0,0)")
+    fig.update_layout(legend=dict(font=dict(size=20)))
+    fig.update_layout(
+        title_text="Sentiment Distribution for Verified Reviews", title_font=dict(size=20))
+
+    # Display the chart on your Streamlit app
+    st.plotly_chart(fig)
+
+    total_observations = sum(counts)
+    st.write(f"Total observations: {total_observations}")
+
+    # Create a table to display counts
+    data = {"Sentiment": sentiments, "Count": counts}
+    df = pd.DataFrame(data)
+    st.table(df)
+
 
 elif selected_feature == "Data Visualization":
     # Data Visualization Content
@@ -82,7 +141,6 @@ elif selected_feature == "Predictive Insights":
     st.header("Predictive Insights")
 # Close the background div for the main content
 st.markdown("</div>", unsafe_allow_html=True)
-
 
 
 # Footer with a subdued color
